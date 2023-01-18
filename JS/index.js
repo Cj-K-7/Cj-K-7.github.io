3b6c53054ae678931041e9014ee079a528bd1ba4
Cj-K-7 <kllt7059@gmail.com>
2023-01-18 21:41:51 +0900

    gyro test2

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,21 @@ const x = document.getElementById("x");
 const y = document.getElementById("y");
 const z = document.getElementById("z");
 
+window.addEventListener("devicemotion", (deviceMotionEvent) => {
+  const { acceleration, accelerationIncludingGravity } = deviceMotionEvent;
+  console.log("accel", acceleration);
+  console.log("gravity", accelerationIncludingGravity);
+});
+
+window.addEventListener("deviceorientation", (deviceOrientationEvent) => {
+  const { absolute, alpha, beta, gamma } = deviceOrientationEvent;
+  console.log(absolute);
+  const z = alpha;
+  const x = beta;
+  const y = gamma;
+  console.log("coordination of device", z, x, y);
+});
+
 if (typeof Gyroscope === "function") {
   const options = { frequency: 60, referenceFrame: "device" };
   const gyroscope = new Gyroscope(options);
