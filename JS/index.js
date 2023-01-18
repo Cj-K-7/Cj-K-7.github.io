29f4d51f2e05dd8ddf1c3b683aaad780647b390b
Cj-K-7 <kllt7059@gmail.com>
2023-01-18 21:24:31 +0900

    gyro test1

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,19 @@
+const x = document.getElementById("x");
+const y = document.getElementById("y");
+const z = document.getElementById("z");
+
 if (typeof Gyroscope === "function") {
   const options = { frequency: 60, referenceFrame: "device" };
   const gyroscope = new Gyroscope(options);
   gyroscope.addEventListener("reading", (e) => {
+    x.innerText = gyroscope.x;
+    y.innerText = gyroscope.y;
+    z.innerText = gyroscope.z;
     console.log(`X-axis ${gyroscope.x}`);
     console.log(`Y-axis ${gyroscope.y}`);
     console.log(`Z-axis ${gyroscope.z}`);
   });
   gyroscope.start();
+} else {
+  console.log("no Gyro");
 }
